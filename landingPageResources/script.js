6f5e2c5e25ab329dd3ddf43ce702d02c1481d9eb
Clay Fox <clayfoxrss@hotmail.co.th>
2019-02-26 20:09:00 +0700

    [Fix] Fixed scrolling issue on landing page
    [Change/Fix]
    -Fixed scrolling issue on landing page when using small devices

--- landingPageResources/script.js
+++ landingPageResources/script.js
@@ -1,3 +1,9 @@
 const body = document.body;
 
 body.setAttribute("style","overflow-y: hidden;"); //disable extra scroll bar from animation
+
+const enableScroll = () => {
+    body.setAttribute("style","overflow-y: auto");
+}
+
+setTimeout(enableScroll, 500);
