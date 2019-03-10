0511c9b673ca8e336658b81060a3c0f313713c9d
Clay Fox <clayfoxrss@hotmail.co.th>
2019-03-10 08:19:12 +0700

    [Add] Added journal page
    -Added journal page
    -Added journal page link to portfolio page

---
+++ journals/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $(".nav-1").click(function () {
+        $(".section").removeClass("section--active");
+        $(".section-1").addClass("section--active");
+    });
+    $(".nav-2").click(function () {
+        $(".section").removeClass("section--active");
+        $(".section-2").addClass("section--active");
+    });
+    $(".nav-3").click(function () {
+        $(".section").removeClass("section--active");
+        $(".section-3").addClass("section--active");
+    });
+    $(".nav-4").click(function () {
+        $(".section").removeClass("section--active");
+        $(".section-4").addClass("section--active");
+    });
+    $(".nav-5").click(function () {
+        $(".section").removeClass("section--active");
+        $(".section-5").addClass("section--active");
+    });
+    $(".nav-6").click(function () {
+        $(".section").removeClass("section--active");
+        $(".section-6").addClass("section--active");
+    });
+
+});
